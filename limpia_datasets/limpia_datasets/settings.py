b92e75086e5eda1dc8eb8deead91d7cb69e98327
Paula <pnunezara@gmail.com>
2026-05-22 15:41:46 -0400

    Fix: Permitiendo CORS para producción

--- limpia_datasets/limpia_datasets/settings.py
+++ limpia_datasets/limpia_datasets/settings.py
@@ -127,4 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Permitir conexiones desde entorno de React
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://limpiadatasets-etl.vercel.app",  # URL real de Vercel
+    "http://localhost:5173",               # Para probar en local
+]
